0aa64e2b691719942a553820b8952311f469403f
Guruh Pamungkas <guruhpamungkas91@gmail.com>
2026-09-02 22:12:50 +0700

    Penyempurnaan Fitur

--- app/api/qris/generate/route.ts
+++ app/api/qris/generate/route.ts
@@ -3,8 +3,11 @@ import { NextResponse } from 'next/server'
 export async function POST(request: Request) {
   try {
     const body = await request.json()
-    // Return response dummy dulu untuk tes
-    return NextResponse.json({ qrUrl: '/mcut.png' })
+    
+    // Ganti <PROJECT_ID> dengan Reference ID Supabase kamu
+    const supabasePublicUrl = 'https://<PROJECT_ID>.supabase.co/storage/v1/object/public/qris-images/mcut.png'
+
+    return NextResponse.json({ qrUrl:"https://nhrrucnttnibqgygzqzz.supabase.co/storage/v1/object/public/qris-images/mcut.png" })
   } catch (error) {
     return NextResponse.json({ error: 'Gagal generate QRIS' }, { status: 500 })
   }
